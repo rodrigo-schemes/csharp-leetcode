c5cb4d873667b5468fd3631bbc205618db67b144
Rodrigo Bitencourt <rodrigoschemes@gmail.com>
2023-12-12 18:44:23 -0300

    Adicionado desafio Convert The Temperature

---
+++ LeetCode/Easy/ConvertTheTemperature.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Easy;
+
+public static class ConvertTheTemperature
+{
+    public static double[] Run(double celsius)
+    {
+        var kelvin = celsius + 273.15;
+        var fahrenheit = celsius * 1.8 + 32;
+
+        return new[] { kelvin, fahrenheit };
+    }
+}
